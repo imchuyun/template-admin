0499dde60df3af5a05864d473e60449386bab6da
imchuyun <69360303+imchuyun@users.noreply.github.com>
2021-06-13 03:14:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function change_url(url) {
     if (url !== "") {
         document.getElementById("myframe").src = url
-        document.getElementById('myiframe').location.reload()
+        document.getElementById('myframe').location.reload()
     }
 }
